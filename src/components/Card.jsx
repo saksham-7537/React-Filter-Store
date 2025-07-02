968b3dc9cd85fca66e6a1b757fcc5f40566e2fbf
Saksham Sethi <sakshamsethi.cs@gmail.com>
2025-07-02 16:46:52 +0530

    Initial commit

---
+++ src/components/Card.jsx
@@ -0,0 +1,33 @@
+import { BsFillBagFill } from "react-icons/bs";
+import { AiFillStar } from "react-icons/ai";
+
+
+const Card = ({ img, title, star, reviews, prevPrice, newPrice }) => {
+  return (
+    <>
+      <section className="card">
+        <img src={img} alt={title} className="card-img" />
+        <div className="card-details">
+          <h3 className="card-title">{title}</h3>
+          <section className="card-reviews">
+            <AiFillStar />
+            <AiFillStar />
+            <AiFillStar />
+            <AiFillStar />
+            <span className="total-reviews">{reviews}</span>
+          </section>
+          <section className="card-price">
+            <div className="price">
+              <del>{prevPrice}</del> {newPrice}
+            </div>
+            <div className="bag">
+              <BsFillBagFill className="bag-icon" />
+            </div>
+          </section>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Card;
